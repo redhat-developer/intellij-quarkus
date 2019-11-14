8f4f8d3935b7b2c0f2f99d7751ca3930601be235
Jeff MAURY <jmaury@redhat.com>
2019-11-14 14:45:24 +0100

    Add pre-check for the LSP plugin presence and minimum version
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/QuarkusPreloadActivity.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/QuarkusPreloadActivity.java
@@ -10,45 +10,50 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.quarkus;
 
-import com.github.gtache.lsp.client.languageserver.serverdefinition.LanguageServerDefinition;
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
 import com.intellij.ide.plugins.PluginManager;
-import com.intellij.ide.plugins.cl.PluginClassLoader;
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.application.PreloadingActivity;
 import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.redhat.devtools.intellij.quarkus.lsp.QuarkusLanguageServerDefinition;
+import com.intellij.openapi.util.Version;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.lang.reflect.Field;
+import static com.redhat.devtools.intellij.quarkus.QuarkusConstants.DISPLAY_CHECK_NOTIFACTION_PROPERTY_NAME;
+import static com.redhat.devtools.intellij.quarkus.QuarkusConstants.NOTIFICATION_GROUP;
 
 public class QuarkusPreloadActivity extends PreloadingActivity {
     @Override
     public void preload(@NotNull ProgressIndicator indicator) {
-        hackClassLoader();
-        IdeaPluginDescriptor descriptor = PluginManager.getPlugin(PluginId.getId("com.redhat.devtools.intellij.quarkus"));
-        File serverPath = new File(descriptor.getPath(), "lib/server/com.redhat.quarkus.ls-0.0.4-SNAPSHOT-uber.jar");
-        String javaHome = System.getProperty("java.home");
-        LanguageServerDefinition.register(new QuarkusLanguageServerDefinition("properties", javaHome + File.separator + "bin" + File.separator + "java", new String[] { "-jar", serverPath.getAbsolutePath().toString()}));
+        checkLSPPlugin();
     }
 
-    private void hackClassLoader() {
-        ClassLoader loader = QuarkusPreloadActivity.class.getClassLoader();
-        if (loader instanceof PluginClassLoader) {
-            try {
-                Field parentsField = loader.getClass().getDeclaredField("myParents");
-                parentsField.setAccessible(true);
-                ClassLoader[] parents = (ClassLoader[]) parentsField.get(loader);
-                if (parents.length > 1) {
-                    ClassLoader first = parents[0];
-                    parents[0] = parents[parents.length - 1];
-                    parents[parents.length - 1] = first;
-                }
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace();
+    private void checkLSPPlugin() {
+        IdeaPluginDescriptor pluginDescriptor = PluginManager.getPlugin(PluginId.getId(QuarkusConstants.LSP_PLUGIN_ID));
+        if (shouldDisplayNotification()) {
+            if (pluginDescriptor == null) {
+                displayNotification("Language Server protocol support not installed, code assist on application.properties will not be provided");
+            } else if (Version.parseVersion(pluginDescriptor.getVersion()).lessThan(1, 5, 5)) {
+                displayNotification("Language Server protocol support installed, code assist on application.properties is available but some issues may happen");
             }
         }
     }
 
+    private void displayNotification(String content) {
+        Notification notification = new Notification(NOTIFICATION_GROUP, AllIcons.General.Warning, "Quarkus", null, content, NotificationType.WARNING, null);
+        Notifications.Bus.notify(notification);
+        resetNotification();
+    }
+
+    private boolean shouldDisplayNotification() {
+        return PropertiesComponent.getInstance().getBoolean(DISPLAY_CHECK_NOTIFACTION_PROPERTY_NAME, true);
+    }
+
+    private void resetNotification() {
+        PropertiesComponent.getInstance().setValue(DISPLAY_CHECK_NOTIFACTION_PROPERTY_NAME, false, true);
+    }
 }
---
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lsp/QuarkusLSPPreloadActivity.java
@@ -0,0 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.quarkus.lsp;
+
+import com.github.gtache.lsp.client.languageserver.serverdefinition.LanguageServerDefinition;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
+import com.intellij.ide.plugins.cl.PluginClassLoader;
+import com.intellij.openapi.application.PreloadingActivity;
+import com.intellij.openapi.extensions.PluginId;
+import com.intellij.openapi.progress.ProgressIndicator;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+
+public class QuarkusLSPPreloadActivity extends PreloadingActivity {
+    private static final Logger LOGGER = LoggerFactory.getLogger(QuarkusLSPPreloadActivity.class);
+
+    @Override
+    public void preload(@NotNull ProgressIndicator indicator) {
+        hackClassLoader();
+        IdeaPluginDescriptor descriptor = PluginManager.getPlugin(PluginId.getId("com.redhat.devtools.intellij.quarkus"));
+        File serverPath = new File(descriptor.getPath(), "lib/server/com.redhat.quarkus.ls-0.0.4-SNAPSHOT-uber.jar");
+        String javaHome = System.getProperty("java.home");
+        LanguageServerDefinition.register(new QuarkusLanguageServerDefinition("properties", javaHome + File.separator + "bin" + File.separator + "java", new String[] { "-jar", serverPath.getAbsolutePath().toString()}));
+    }
+
+    private void hackClassLoader() {
+        ClassLoader loader = QuarkusLSPPreloadActivity.class.getClassLoader();
+        if (loader instanceof PluginClassLoader) {
+            try {
+                Field parentsField = loader.getClass().getDeclaredField("myParents");
+                parentsField.setAccessible(true);
+                ClassLoader[] parents = (ClassLoader[]) parentsField.get(loader);
+                if (parents.length > 1) {
+                    ClassLoader first = parents[0];
+                    parents[0] = parents[parents.length - 1];
+                    parents[parents.length - 1] = first;
+                }
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                LOGGER.error(e.getLocalizedMessage(), e);
+            }
+        }
+    }
+
+}
