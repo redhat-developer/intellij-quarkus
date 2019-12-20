bd80c89d6833c4bdae13a0a8ffc3aa95792f0dbe
Jeff MAURY <jmaury@redhat.com>
2019-12-20 07:43:52 +0100

    Fix LSP messages by hacking lsp streams
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/QuarkusPreloadActivity.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/QuarkusPreloadActivity.java
@@ -12,15 +12,12 @@ package com.redhat.devtools.intellij.quarkus;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.ide.plugins.IdeaPluginDescriptor;
-import com.intellij.ide.plugins.PluginManager;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.application.PreloadingActivity;
-import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.util.Version;
 import org.jetbrains.annotations.NotNull;
 
 import static com.redhat.devtools.intellij.quarkus.QuarkusConstants.DISPLAY_CHECK_NOTIFACTION_PROPERTY_NAME;
@@ -33,11 +30,11 @@ public class QuarkusPreloadActivity extends PreloadingActivity {
     }
 
     private void checkLSPPlugin() {
-        IdeaPluginDescriptor pluginDescriptor = PluginManager.getPlugin(PluginId.getId(QuarkusConstants.LSP_PLUGIN_ID));
+        IdeaPluginDescriptor pluginDescriptor = PluginHelper.getLSPPluginDescriptor();
         if (shouldDisplayNotification()) {
             if (pluginDescriptor == null) {
                 displayNotification("Language Server protocol support not installed, code assist on application.properties will not be provided");
-            } else if (Version.parseVersion(pluginDescriptor.getVersion()).lessThan(1, 5, 5)) {
+            } else if (PluginHelper.isLSPPluginPre154(pluginDescriptor)) {
                 displayNotification("Language Server protocol support installed, code assist on application.properties is available but some issues may happen");
             }
         }
