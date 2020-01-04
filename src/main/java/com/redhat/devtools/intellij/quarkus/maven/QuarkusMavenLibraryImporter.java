f991f8ff4f66fad62b1f34ad1ce0e352e9e36581
Jeff MAURY <jmaury@redhat.com>
2020-01-04 12:11:13 +0100

    Add MavenImporter for newly created Maven projects
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/quarkus/maven/QuarkusMavenLibraryImporter.java
@@ -0,0 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.quarkus.maven;
+
+import com.intellij.openapi.externalSystem.service.project.IdeModifiableModelsProvider;
+import com.intellij.openapi.module.Module;
+import com.intellij.util.containers.ContainerUtil;
+import org.jetbrains.idea.maven.importing.MavenImporter;
+import org.jetbrains.idea.maven.importing.MavenRootModelAdapter;
+import org.jetbrains.idea.maven.project.MavenProject;
+import org.jetbrains.idea.maven.project.MavenProjectChanges;
+import org.jetbrains.idea.maven.project.MavenProjectsProcessorTask;
+import org.jetbrains.idea.maven.project.MavenProjectsTree;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class QuarkusMavenLibraryImporter extends MavenImporter {
+    private static final List<String> SUPPORTED_PACKAGINGS = ContainerUtil.immutableList(new String[]{"jar", "war"});
+
+    public QuarkusMavenLibraryImporter() {
+        super("io.quarkus", "quarkus-maven-plugin");
+    }
+
+    @Override
+    public void getSupportedPackagings(Collection<String> result) {
+        result.addAll(SUPPORTED_PACKAGINGS);
+    }
+
+    @Override
+    public void preProcess(Module module, MavenProject mavenProject, MavenProjectChanges changes, IdeModifiableModelsProvider modifiableModelsProvider) {
+    }
+
+    @Override
+    public void process(IdeModifiableModelsProvider modifiableModelsProvider, Module module, MavenRootModelAdapter rootModel, MavenProjectsTree mavenModel, MavenProject mavenProject, MavenProjectChanges changes, Map<MavenProject, String> mavenProjectToModuleName, List<MavenProjectsProcessorTask> postTasks) {
+        postTasks.add(new QuarkusMavenProjectsProcessTask(module));
+    }
+}
---
+++ src/main/java/com/redhat/devtools/intellij/quarkus/maven/QuarkusMavenProjectsProcessTask.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.quarkus.maven;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.DependencyScope;
+import com.intellij.openapi.roots.ModuleRootModificationUtil;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.redhat.devtools.intellij.quarkus.tool.ToolDelegate;
+import org.jetbrains.idea.maven.project.MavenConsole;
+import org.jetbrains.idea.maven.project.MavenEmbeddersManager;
+import org.jetbrains.idea.maven.project.MavenProjectsProcessorTask;
+import org.jetbrains.idea.maven.utils.MavenProcessCanceledException;
+import org.jetbrains.idea.maven.utils.MavenProgressIndicator;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.redhat.devtools.intellij.quarkus.QuarkusConstants.QUARKUS_DEPLOYMENT_LIBRARY_NAME;
+
+public class QuarkusMavenProjectsProcessTask implements MavenProjectsProcessorTask {
+    private final Module module;
+
+    public QuarkusMavenProjectsProcessTask(Module module) {
+        this.module = module;
+    }
+
+    @Override
+    public void perform(Project project, MavenEmbeddersManager embeddersManager, MavenConsole console, MavenProgressIndicator indicator) throws MavenProcessCanceledException {
+        ToolDelegate toolDelegate = ToolDelegate.getDelegate(module);
+        if (toolDelegate != null) {
+            List<VirtualFile> deploymentFiles = toolDelegate.getDeploymentFiles(module);
+            if (!deploymentFiles.isEmpty()) {
+                ModuleRootModificationUtil.addModuleLibrary(module, QUARKUS_DEPLOYMENT_LIBRARY_NAME, deploymentFiles.stream().map(file -> file.getUrl()).collect(Collectors.toList()), Collections.emptyList(), DependencyScope.PROVIDED);
+            }
+        }
+    }
+}
