e5470048061414b3682d2ebd842282ae3ac62aa4
Jeff MAURY <jmaury@redhat.com>
2020-02-28 08:14:27 +0100

    Extensions with status other than preview should get status displayed
    Fixes #58
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/test/java/com/redhat/devtools/intellij/quarkus/module/QuarkusExtensionsTest.java
+++ src/test/java/com/redhat/devtools/intellij/quarkus/module/QuarkusExtensionsTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2019 Red Hat, Inc.
+ * Copyright (c) 2019-2020 Red Hat, Inc.
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
@@ -43,5 +43,14 @@ public class QuarkusExtensionsTest {
         assertEquals(1, model.getCategories().get(0).getExtensions().size());
         assertEquals("Amazon DynamoDB client", model.getCategories().get(0).getExtensions().get(0).getName());
         assertEquals("Amazon DynamoDB client (Preview)", model.getCategories().get(0).getExtensions().get(0).asLabel());
+    }
+
+    @Test
+    public void checkExperimentalExtension() throws IOException {
+        QuarkusModel model = new QuarkusModel(load("/single-experimental-extension.json"));
+        assertEquals(1, model.getCategories().size());
+        assertEquals(1, model.getCategories().get(0).getExtensions().size());
+        assertEquals("Amazon DynamoDB client", model.getCategories().get(0).getExtensions().get(0).getName());
+        assertEquals("Amazon DynamoDB client (Experimental)", model.getCategories().get(0).getExtensions().get(0).asLabel());
     }
 }
