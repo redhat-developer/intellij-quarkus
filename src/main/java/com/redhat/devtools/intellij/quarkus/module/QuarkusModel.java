09d152ac37e9ad7d44d213490634e526702a01b7
Jeff MAURY <jmaury@redhat.com>
2020-06-03 17:11:55 +0200

    Toggling an extension's checkbox should toggle the same extensions' checkboxes in all categories
    Fixes #96
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/module/QuarkusModel.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/module/QuarkusModel.java
@@ -12,20 +12,28 @@ package com.redhat.devtools.intellij.quarkus.module;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class QuarkusModel {
     private List<QuarkusCategory> categories = new ArrayList<>();
 
     public QuarkusModel(List<QuarkusExtension> extensions) {
         Collections.sort(extensions, (e1, e2) -> e1.getOrder() - e1.getOrder());
+        Map<String, QuarkusExtension> extensionIds = new HashMap<>();
         final QuarkusCategory[] currentCategory = {null};
         extensions.forEach(e -> {
             if (currentCategory[0] == null || !e.getCategory().equals(currentCategory[0].getName())) {
                 currentCategory[0] = new QuarkusCategory(e.getCategory());
                 categories.add(currentCategory[0]);
             }
-            currentCategory[0].getExtensions().add(e);
+            if (extensionIds.containsKey(e.getId())) {
+                currentCategory[0].getExtensions().add(extensionIds.get(e.getId()));
+            } else {
+                currentCategory[0].getExtensions().add(e);
+                extensionIds.put(e.getId(), e);
+            }
         });
     }
     public List<QuarkusCategory> getCategories() {
