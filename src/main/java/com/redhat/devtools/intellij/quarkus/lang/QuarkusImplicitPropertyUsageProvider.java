b4cfe38b4c50ac55925047f354fff2c3133a382e
Jeff MAURY <jmaury@redhat.com>
2020-06-19 16:54:50 +0200

    Remove warnings about not used property in application.properties
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lang/QuarkusImplicitPropertyUsageProvider.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.quarkus.lang;
+
+import com.intellij.codeInspection.unused.ImplicitPropertyUsageProvider;
+import com.intellij.lang.properties.psi.Property;
+
+public class QuarkusImplicitPropertyUsageProvider extends ImplicitPropertyUsageProvider {
+    @Override
+    protected boolean isUsed(Property property) {
+        return QuarkusIconProvider.isQuarkusPropertiesFile(property.getContainingFile().getVirtualFile(), property.getProject());
+    }
+}
