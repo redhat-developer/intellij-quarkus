72b335ef4e80c889ac514d105e645b86f18efda7
Jeff MAURY <jmaury@redhat.com>
2020-07-24 19:08:57 +0200

    Fix deadlock while computing the completions (#194)
    Fixes #185
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/operations/completion/LSContentAssistProcessor.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/operations/completion/LSContentAssistProcessor.java
@@ -10,7 +10,6 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.quarkus.lsp4ij.operations.completion;
 
-import com.google.common.base.Strings;
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionResultSet;
@@ -34,10 +33,7 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -45,22 +41,18 @@ import java.util.stream.Collectors;
 public class LSContentAssistProcessor extends CompletionContributor {
     private static final Logger LOGGER = LoggerFactory.getLogger(LSContentAssistProcessor.class);
 
-    private CompletableFuture<List<LanguageServer>> completionLanguageServersFuture;
-    private final Object completionTriggerCharsSemaphore = new Object();
-    private char[] completionTriggerChars = new char[0];
-
     @Override
     public void fillCompletionVariants(@NotNull CompletionParameters parameters, @NotNull CompletionResultSet result) {
         Document document = parameters.getEditor().getDocument();
         Editor editor = parameters.getEditor();
         Project project = parameters.getOriginalFile().getProject();
         int offset = parameters.getOffset();
-        initiateLanguageServers(project, document);
+        CompletableFuture<List<LanguageServer>> completionLanguageServersFuture = initiateLanguageServers(project, document);
         CompletionParams param;
         try {
             param = LSPIJUtils.toCompletionParams(LSPIJUtils.toUri(document), offset, document);
             List<LookupElement> proposals = Collections.synchronizedList(new ArrayList<>());
-            this.completionLanguageServersFuture
+            completionLanguageServersFuture
                     .thenComposeAsync(languageServers -> CompletableFuture.allOf(languageServers.stream()
                             .map(languageServer -> languageServer.getTextDocumentService().completion(param)
                                     .thenAcceptAsync(completion -> proposals
@@ -95,49 +87,14 @@ public class LSContentAssistProcessor extends CompletionContributor {
         return LookupElementBuilder.create("Error while computing completion", "");
     }
 
-    private void initiateLanguageServers(Project project, Document document) {
-        if (this.completionLanguageServersFuture != null) {
-            try {
-                this.completionLanguageServersFuture.cancel(true);
-            } catch (CancellationException ex) {
-                // nothing
-            }
-        }
-        this.completionTriggerChars = new char[0];
-
-        this.completionLanguageServersFuture = LanguageServiceAccessor.getInstance(project).getLanguageServers(document,
+    private CompletableFuture<List<LanguageServer>> initiateLanguageServers(Project project, Document document) {
+        return LanguageServiceAccessor.getInstance(project).getLanguageServers(document,
                 capabilities -> {
                     CompletionOptions provider = capabilities.getCompletionProvider();
                     if (provider != null) {
-                        synchronized (this.completionTriggerCharsSemaphore) {
-                            this.completionTriggerChars = mergeTriggers(this.completionTriggerChars,
-                                    provider.getTriggerCharacters());
-                        }
                         return true;
                     }
                     return false;
                 });
-    }
-
-    private static char[] mergeTriggers(char[] initialArray, Collection<String> additionalTriggers) {
-        if (initialArray == null) {
-            initialArray = new char[0];
-        }
-        if (additionalTriggers == null) {
-            additionalTriggers = Collections.emptySet();
-        }
-        Set<Character> triggers = new HashSet<>();
-        for (char c : initialArray) {
-            triggers.add(Character.valueOf(c));
-        }
-        additionalTriggers.stream().filter(s -> !Strings.isNullOrEmpty(s))
-                .map(triggerChar -> Character.valueOf(triggerChar.charAt(0))).forEach(triggers::add);
-        char[] res = new char[triggers.size()];
-        int i = 0;
-        for (Character c : triggers) {
-            res[i] = c.charValue();
-            i++;
-        }
-        return res;
     }
 }
