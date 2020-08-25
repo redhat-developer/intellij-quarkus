8b743ddbb47071c4e62c648057b4a13b3e7e2edf
Jeff MAURY <jmaury@redhat.com>
2020-08-25 10:28:39 +0200

    Remove class scanning in content assist after the = sign (#212)
    Fixes #211
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/lang/QuarkusIconProvider.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lang/QuarkusIconProvider.java
@@ -10,16 +10,12 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.quarkus.lang;
 
-import com.intellij.facet.FacetManager;
 import com.intellij.ide.IconProvider;
 import com.intellij.lang.properties.psi.PropertiesFile;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleUtilCore;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
-import com.redhat.devtools.intellij.quarkus.facet.QuarkusFacet;
+import com.redhat.devtools.intellij.quarkus.QuarkusModuleUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -28,20 +24,12 @@ import javax.swing.Icon;
 public class QuarkusIconProvider extends IconProvider {
     private static final Icon QUARKUS_ICON = IconLoader.findIcon("/quarkus_icon_rgb_16px_default.png", QuarkusIconProvider.class);
 
-    static boolean isQuarkusPropertiesFile(VirtualFile file, Project project) {
-        if ("application.properties".equals("application.properties")) {
-            Module module = ModuleUtilCore.findModuleForFile(file, project);
-            return module != null && FacetManager.getInstance(module).getFacetByType(QuarkusFacet.FACET_TYPE_ID) != null;
-        }
-        return false;
-    }
-
     @Nullable
     @Override
     public Icon getIcon(@NotNull PsiElement element, int flags) {
         if (element instanceof PropertiesFile) {
             VirtualFile file = element.getContainingFile().getVirtualFile();
-            if (isQuarkusPropertiesFile(file, element.getProject())) {
+            if (QuarkusModuleUtil.isQuarkusPropertiesFile(file, element.getProject())) {
                     return QUARKUS_ICON;
             }
         }
