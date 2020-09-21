d2fabfa5743ff51c3cdd5075e4e1aaea8d1d76c0
Jeff MAURY <jmaury@redhat.com>
2020-09-21 17:56:51 +0200

    Switch to LSP4MP 0.1.0
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/lsp/QuarkusServer.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lsp/QuarkusServer.java
@@ -21,7 +21,7 @@ import java.util.Arrays;
 public class QuarkusServer extends ProcessStreamConnectionProvider {
     public QuarkusServer() {
         IdeaPluginDescriptor descriptor = PluginManager.getPlugin(PluginId.getId("com.redhat.devtools.intellij.quarkus"));
-        File lsp4mpServerPath = new File(descriptor.getPath(), "lib/server/org.eclipse.lsp4mp.ls-0.9.0-SNAPSHOT-uber.jar");
+        File lsp4mpServerPath = new File(descriptor.getPath(), "lib/server/org.eclipse.lsp4mp.ls-0.1.0-SNAPSHOT-uber.jar");
         File quarkusServerPath = new File(descriptor.getPath(), "lib/server/com.redhat.quarkus.ls-0.9.0-SNAPSHOT.jar");
         String javaHome = System.getProperty("java.home");
         setCommands(Arrays.asList(javaHome + File.separator + "bin" + File.separator + "java", "-jar",
