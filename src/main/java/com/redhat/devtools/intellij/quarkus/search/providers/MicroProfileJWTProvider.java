30910dc44ceb049f6c839e620d1f94ef7e4a0b36
Jeff MAURY <jmaury@redhat.com>
2020-09-29 15:49:12 +0200

    Add MicroProfile JWT support
    Fixes #248
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/quarkus/search/providers/MicroProfileJWTProvider.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+* Copyright (c) 2020 IBM Corporation and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+* which is available at https://www.apache.org/licenses/LICENSE-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+*
+* Contributors:
+*     IBM Corporation - initial API and implementation
+*******************************************************************************/
+
+package com.redhat.devtools.intellij.quarkus.search.providers;
+
+import com.intellij.openapi.module.Module;
+import com.redhat.devtools.intellij.quarkus.search.SearchContext;
+import com.redhat.devtools.intellij.quarkus.search.core.utils.PsiTypeUtils;
+
+import static com.redhat.devtools.intellij.quarkus.search.internal.health.MicroProfileHealthConstants.LIVENESS_ANNOTATION;
+import static com.redhat.devtools.intellij.quarkus.search.providers.MicroProfileJWTConstants.JWT_CLAIM;
+
+/**
+ * Properties provider that provides static MicroProfile JTW properties
+ *
+ * @author Kathryn Kodama
+ *
+ * @see <a href="https://github.com/eclipse/microprofile-jwt-auth/blob/master/spec/src/main/asciidoc/configuration.asciidoc"></a>https://github.com/eclipse/microprofile-jwt-auth/blob/master/spec/src/main/asciidoc/configuration.asciidoc</a>
+ */
+public class MicroProfileJWTProvider extends AbstractStaticPropertiesProvider {
+
+    public MicroProfileJWTProvider() {
+        super("/static-properties/mp-jwt-metadata.json");
+    }
+
+    @Override
+    protected boolean isAdaptedFor(SearchContext context) {
+        // Check if MicroProfile JWT exists in classpath
+        Module javaProject = context.getModule();
+        return (PsiTypeUtils.findType(javaProject, JWT_CLAIM) != null);
+    }
+
+}
---
+++ src/test/java/com/redhat/devtools/intellij/quarkus/gradle/GradleMicroProfileJWTTest.java
@@ -0,0 +1,75 @@
+/*******************************************************************************
+* Copyright (c) 2019 Red Hat Inc. and others.
+* All rights reserved. This program and the accompanying materials
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
+package com.redhat.devtools.intellij.quarkus.gradle;
+
+import com.redhat.devtools.intellij.quarkus.search.PropertiesManager;
+import com.redhat.devtools.intellij.quarkus.search.PsiUtilsImpl;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.lsp4mp.commons.ClasspathKind;
+import org.eclipse.lsp4mp.commons.DocumentFormat;
+import org.eclipse.lsp4mp.commons.MicroProfileProjectInfo;
+import org.eclipse.lsp4mp.commons.MicroProfilePropertiesScope;
+import org.junit.Test;
+
+import java.io.File;
+
+import static com.redhat.devtools.intellij.quarkus.module.MicroProfileAssert.assertProperties;
+import static com.redhat.devtools.intellij.quarkus.module.MicroProfileAssert.p;
+
+/**
+ * Test the availability of the MicroProfile JWT properties
+ *
+ * @author Kathryn Kodama
+ *
+ */
+public class GradleMicroProfileJWTTest extends GradleTestCase {
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		FileUtils.copyDirectory(new File("projects/gradle/microprofile-jwt"), new File(getProjectPath()));
+		importProject();
+	}
+
+	@Test
+	public void testMicroprofileHealth() throws Exception {
+
+		MicroProfileProjectInfo infoFromClasspath = PropertiesManager.getInstance().getMicroProfileProjectInfo(getModule("microprofile-jwt.main"), MicroProfilePropertiesScope.SOURCES_AND_DEPENDENCIES, ClasspathKind.SRC, PsiUtilsImpl.getInstance(), DocumentFormat.PlainText);
+
+		assertProperties(infoFromClasspath,
+				// confirm properties are being merged with force, should not overwrite properties coming from ConfigProperty provider
+
+				p(null, "mp.jwt.verify.issuer", "java.lang.String", null,
+						true, "io.smallrye.jwt.config.JWTAuthContextInfoProvider", "mpJwtIssuer", null, 0, "NONE"),
+
+				// properties coming from static JSON
+
+				p("microprofile-jwt-api", "mp.jwt.verify.publickey.algorithm", "java.lang.String",
+						"Configuration property to specify the Public Key Signature Algorithm property. The value can be set to either `RS256` or `ES256`, `RS256` is the default value.",
+						true, null, null, null, 0, null),
+
+				p("microprofile-jwt-api", "mp.jwt.decrypt.key.location", "java.lang.String",
+						"Configuration property to specify the relative path or full URL of the decryption key.",
+						true, null, null, null, 0, null),
+
+				p("microprofile-jwt-api", "mp.jwt.token.header", "java.lang.String",
+						"Configuration property to specify the `HTTP` header name expected to contain the JWT token.",
+						true, null, null, null, 0, null),
+
+				p("microprofile-jwt-api", "mp.jwt.token.cookie", "java.lang.String",
+						"Configuration property to specify the Cookie name (default is `Bearer`) expected to contain the JWT token. This configuration will be ignored unless `mp.jwt.token.header` is set to `Cookie`.",
+						true, null, null, null, 0, null),
+
+				p("microprofile-jwt-api", "mp.jwt.verify.audiences", "java.lang.String",
+						"Configuration property to specify the list of allowable value(s) for the `aud` claim, separated by commas. If specified, MP-JWT claim must be present and match one of the values.",
+						true, null, null, null, 0, null)
+		);
+	}
+}
