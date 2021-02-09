b2d3b093b03e7a080852811a51218582fef43281
Jeff MAURY <jmaury@redhat.com>
2021-02-09 13:47:26 +0100

    fix: switch URLs to https: (#361)
    Fixes #199
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/search/PsiUtilsImpl.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/search/PsiUtilsImpl.java
@@ -3,7 +3,7 @@
  * Distributed under license by Red Hat, Inc. All rights reserved.
  * This program is made available under the terms of the
  * Eclipse Public License v2.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v20.html
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
  *
  * Contributors:
  * Red Hat, Inc. - initial API and implementation
