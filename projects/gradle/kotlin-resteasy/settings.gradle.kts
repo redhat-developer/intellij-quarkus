242d770d21c2e03b738e065e031416c46b1b7d5e
Jeff MAURY <jmaury@redhat.com>
2021-08-09 09:14:12 +0200

    Add unit test
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ projects/gradle/kotlin-resteasy/settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    val quarkusPluginVersion: String by settings
+    val quarkusPluginId: String by settings
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id(quarkusPluginId) version quarkusPluginVersion
+    }
+}
+rootProject.name="kotlin-resteasy"
