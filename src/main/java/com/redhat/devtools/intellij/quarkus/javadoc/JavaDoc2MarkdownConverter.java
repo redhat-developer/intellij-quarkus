7e3b1b4d39f8b4944516ba40fc512567f9be4824
Jeff MAURY <jmaury@redhat.com>
2022-05-27 17:26:06 +0200

    fix: Upgrade to jsoup 1.14.2 (#579)
    * fix: Upgrade to jsoup 1.14.2
    
    Fixes #578
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/javadoc/JavaDoc2MarkdownConverter.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/javadoc/JavaDoc2MarkdownConverter.java
@@ -15,8 +15,7 @@ import java.io.Reader;
 import java.lang.reflect.Field;
 
 import org.jsoup.safety.Cleaner;
-import org.jsoup.safety.Whitelist;
-
+import org.jsoup.safety.Safelist;
 import com.overzealous.remark.Options;
 import com.overzealous.remark.Options.Tables;
 import com.overzealous.remark.Remark;
@@ -48,13 +47,13 @@ public class JavaDoc2MarkdownConverter extends AbstractJavaDocConverter {
 
             Cleaner c = (Cleaner) cleanerField.get(remark);
 
-            Field whitelistField = Cleaner.class.getDeclaredField("whitelist");
-            whitelistField.setAccessible(true);
+            Field safelistField = Cleaner.class.getDeclaredField("safelist");
+            safelistField.setAccessible(true);
 
-            Whitelist w = (Whitelist) whitelistField.get(c);
+            Safelist s = (Safelist) safelistField.get(c);
 
-            w.addProtocols("a", "href", "file", "jdt");
-            w.addProtocols("img", "src", "file");
+            s.addProtocols("a", "href", "file", "jdt");
+            s.addProtocols("img", "src", "file");
         } catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
             LOGGER.error("Unable to modify jsoup to include file and jdt protocols", e);
         }
