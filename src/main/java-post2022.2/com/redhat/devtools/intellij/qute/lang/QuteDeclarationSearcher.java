237c7757e4d616e7c1dd106711bd5cfd078eb529
Jeff MAURY <jmaury@redhat.com>
2022-08-09 08:20:07 +0200

    build: Add IntelliJ 2022.2 (#627)
    * build: Add IntelliJ 2022.2
    * Fix compilation error due to signature change
    
    Fixes #626
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/main/java-post2022.2/com/redhat/devtools/intellij/qute/lang/QuteDeclarationSearcher.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.qute.lang;
+
+import com.intellij.lang.Language;
+import com.intellij.lang.LanguageUtil;
+import com.intellij.pom.PomDeclarationSearcher;
+import com.intellij.pom.PomTarget;
+import com.intellij.psi.DelegatePsiTarget;
+import com.intellij.psi.PsiElement;
+import com.intellij.util.Consumer;
+import org.jetbrains.annotations.NotNull;
+
+public class QuteDeclarationSearcher extends PomDeclarationSearcher {
+    @Override
+    public void findDeclarationsAt(@NotNull PsiElement element, int offsetInElement, Consumer<? super PomTarget> consumer) {
+        Language lang = LanguageUtil.getLanguageForPsi(element.getProject(), element.getContainingFile().getVirtualFile());
+        if (lang == QuteLanguage.INSTANCE) {
+            consumer.consume(new DelegatePsiTarget(element));
+        }
+    }
+}
