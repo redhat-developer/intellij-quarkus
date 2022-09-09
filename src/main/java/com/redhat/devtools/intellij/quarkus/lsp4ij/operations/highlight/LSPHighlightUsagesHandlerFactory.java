974d3013fe94a8a5ec1b6a971406a77d0058489b
Jeff MAURY <jmaury@redhat.com>
2022-09-09 13:38:57 +0200

    feat: Support for textDocument/documentHighlight (#657)
    Fixes #647
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/operations/highlight/LSPHighlightUsagesHandlerFactory.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.quarkus.lsp4ij.operations.highlight;
+
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase;
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactory;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class LSPHighlightUsagesHandlerFactory implements HighlightUsagesHandlerFactory {
+    @Override
+    public @Nullable HighlightUsagesHandlerBase createHighlightUsagesHandler(@NotNull Editor editor, @NotNull PsiFile file) {
+        return new LSPHighlightUsagesHandler(editor, file);
+    }
+}
