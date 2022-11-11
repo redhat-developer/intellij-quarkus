1d059d61198d347fcd69eeb904d77a2cd0e4035b
Jeff MAURY <jmaury@redhat.com>
2022-11-11 09:57:50 +0100

    fix: Quarkus Tools brakes Intellijs syntax highlighting (#680)
    Fixes #677
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/operations/highlight/LSPHighlightUsagesHandler.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/operations/highlight/LSPHighlightUsagesHandler.java
@@ -10,81 +10,28 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.quarkus.lsp4ij.operations.highlight;
 
-import com.intellij.codeInsight.TargetElementUtil;
 import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.progress.ProgressManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.Consumer;
-import com.redhat.devtools.intellij.quarkus.lsp4ij.LSPIJUtils;
-import com.redhat.devtools.intellij.quarkus.lsp4ij.LanguageServiceAccessor;
-import org.eclipse.lsp4j.DocumentHighlight;
-import org.eclipse.lsp4j.DocumentHighlightParams;
-import org.eclipse.lsp4j.Position;
-import org.eclipse.lsp4j.TextDocumentIdentifier;
 import org.jetbrains.annotations.NotNull;
 
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class LSPHighlightUsagesHandler extends HighlightUsagesHandlerBase<PsiElement> {
     private static final Logger LOGGER = Logger.getLogger(LSPHighlightUsagesHandler.class.getName());
+    private final List<PsiElement> targets;
 
-    public LSPHighlightUsagesHandler(Editor editor, PsiFile file) {
+    public LSPHighlightUsagesHandler(Editor editor, PsiFile file, List<PsiElement> targets) {
         super(editor, file);
+        this.targets = targets;
     }
 
     @Override
     public @NotNull List<PsiElement> getTargets() {
-        List<PsiElement> elements = new ArrayList<>();
-        try {
-            int offset = TargetElementUtil.adjustOffset(myFile, myEditor.getDocument(), myEditor.getCaretModel().getOffset());
-            Document document = myEditor.getDocument();
-            Position position;
-            position = LSPIJUtils.toPosition(offset, document);
-            URI uri = LSPIJUtils.toUri(document);
-            if(uri == null) {
-                return Collections.emptyList();
-            }
-            ProgressManager.checkCanceled();
-            TextDocumentIdentifier identifier = new TextDocumentIdentifier(uri.toString());
-            DocumentHighlightParams params = new DocumentHighlightParams(identifier, position);
-            BlockingDeque<DocumentHighlight> highlights = new LinkedBlockingDeque<>();
-            CompletableFuture<Void> future = LanguageServiceAccessor.getInstance(myEditor.getProject()).getLanguageServers(document,
-                            capabilities -> LSPIJUtils.hasCapability(capabilities.getDocumentHighlightProvider()))
-                    .thenAcceptAsync(languageServers ->
-                            CompletableFuture.allOf(languageServers.stream()
-                                    .map(languageServer -> languageServer.getTextDocumentService().documentHighlight(params))
-                                    .map(request -> request.thenAcceptAsync(result -> {
-                                        if (result != null) {
-                                            result.forEach(hightlight -> highlights.add(hightlight));
-                                        }
-                                    })).toArray(CompletableFuture[]::new)));
-            while (!future.isDone() || !highlights.isEmpty()) {
-                ProgressManager.checkCanceled();
-                DocumentHighlight highlight = highlights.poll(25, TimeUnit.MILLISECONDS);
-                if (highlight != null) {
-                    int highlightOffset = LSPIJUtils.toOffset(highlight.getRange().getStart(), document);
-                    PsiElement element = myFile.findElementAt(highlightOffset);
-                    if (element != null) {
-                        elements.add(element);
-                    }
-                }
-            }
-        } catch (InterruptedException e) {
-           LOGGER.log(Level.WARNING, e, e::getLocalizedMessage);
-        }
-        return elements;
+        return targets;
     }
 
     @Override
