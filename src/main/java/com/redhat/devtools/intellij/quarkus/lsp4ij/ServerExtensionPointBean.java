81e77bb4eeaad434f344de7a10eeeb76670d9da2
Jeff MAURY <jmaury@redhat.com>
2022-12-08 17:15:59 +0100

    fix: Quarkus Tools is broken on IU-2022.3 (#706)
    * fix: Quarkus Tools is broken on IU-2002.3
    
    Fixes #704
    
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/ServerExtensionPointBean.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/ServerExtensionPointBean.java
@@ -1,10 +1,13 @@
 package com.redhat.devtools.intellij.quarkus.lsp4ij;
 
-import com.intellij.openapi.extensions.AbstractExtensionPointBean;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.extensions.ExtensionPointName;
+import com.intellij.serviceContainer.BaseKeyedLazyInstance;
 import com.intellij.util.xmlb.annotations.Attribute;
+import com.redhat.devtools.intellij.quarkus.lsp4ij.server.StreamConnectionProvider;
+import org.jetbrains.annotations.Nullable;
 
-public class ServerExtensionPointBean extends AbstractExtensionPointBean {
+public class ServerExtensionPointBean extends BaseKeyedLazyInstance<StreamConnectionProvider> {
     public static final ExtensionPointName<ServerExtensionPointBean> EP_NAME = ExtensionPointName.create("com.redhat.devtools.intellij.quarkus.server");
 
     @Attribute("id")
@@ -18,18 +21,31 @@ public class ServerExtensionPointBean extends AbstractExtensionPointBean {
 
     @Attribute("clientImpl")
     public String clientImpl;
+    private Class clientClass;
 
     @Attribute("serverInterface")
     public String serverInterface;
+    private Class serverClass;
 
     @Attribute("singleton")
     public boolean singleton;
 
     public Class getClientImpl() throws ClassNotFoundException {
-        return findClass(clientImpl);
+        if (clientClass == null) {
+            clientClass = getPluginDescriptor().getPluginClassLoader().loadClass(clientImpl);
+        }
+        return clientClass;
     }
 
     public Class getServerInterface() throws ClassNotFoundException {
-        return findClass(serverInterface);
+        if (serverClass == null) {
+            serverClass = getPluginDescriptor().getPluginClassLoader().loadClass(serverInterface);
+        }
+        return serverClass;
+    }
+
+    @Override
+    protected @Nullable String getImplementationClassName() {
+        return clazz;
     }
 }
