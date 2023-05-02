ff92b9362338fe2b293c28dec280502def290264
azerr <azerr@redhat.com>
2023-05-02 10:45:38 +0200

    fix: Duplicate validation when project is open / closed and reopened (#834)
    Fixes #834
    
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/ConnectDocumentToLanguageServerSetupParticipant.java
+++ src/main/java/com/redhat/devtools/intellij/quarkus/lsp4ij/ConnectDocumentToLanguageServerSetupParticipant.java
@@ -24,7 +24,11 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.URI;
 
+/**
+ * Track file opened / closed to start language servers / disconnect file from language servers.
+ */
 public class ConnectDocumentToLanguageServerSetupParticipant implements ProjectComponent, FileEditorManagerListener {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ConnectDocumentToLanguageServerSetupParticipant.class);
 
     private Project project;
@@ -42,21 +46,32 @@ public class ConnectDocumentToLanguageServerSetupParticipant implements ProjectC
     public void fileOpened(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
         Document document = FileDocumentManager.getInstance().getDocument(file);
         if (document != null) {
-            LanguageServiceAccessor.getInstance(source.getProject()).getLanguageServers(document, capabilities -> true);
+            // Force the start of all languages servers mapped with the given file
+            LanguageServiceAccessor.getInstance(source.getProject())
+                    .getLanguageServers(document, capabilities -> true);
         }
     }
 
     @Override
     public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile file) {
         URI uri = LSPIJUtils.toUri(file);
-        if (file != null) {
+        if (uri != null) {
             try {
-                LanguageServiceAccessor.getInstance(source.getProject()).getLSWrappers(file, capabilities -> true).forEach(wrapper -> wrapper.disconnect(uri));
-            } catch (IOException e) {
-                LOGGER.warn(e.getLocalizedMessage(), e);
+                // TODO: revisit this code, because it can restart language servers
+                // when a diagnostics is published after the file is closed and the project is closed
+                // See https://github.com/redhat-developer/intellij-quarkus/issues/840
+                // Remove the cached file wrapper if needed
+                LSPVirtualFileWrapper.dispose(file);
+                // Disconnect the given file from all language servers
+                LanguageServiceAccessor.getInstance(source.getProject())
+                        .getLSWrappers(file, capabilities -> true)
+                        .forEach(
+                                wrapper -> wrapper.disconnect(uri)
+                        );
+            } catch (Exception e) {
+                LOGGER.warn("Error while disconnecting the file '" + uri + "' from all language servers", e);
             }
         }
-
     }
 
 }
