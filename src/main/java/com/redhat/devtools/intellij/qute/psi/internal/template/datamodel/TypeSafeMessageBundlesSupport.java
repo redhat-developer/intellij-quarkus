0ec40d89a95c5f38eff3f5170fd8390911b34796
azerr <azerr@redhat.com>
2023-09-04 03:26:40 +0200

    feat: Partial support for Qute Type-safe Message Bundles (Java classes only)
    Fixes #1139
    
    Signed-off-by: azerr <azerr@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/qute/psi/internal/template/datamodel/TypeSafeMessageBundlesSupport.java
@@ -0,0 +1,142 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package com.redhat.devtools.intellij.qute.psi.internal.template.datamodel;
+
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.psi.*;
+import com.redhat.devtools.intellij.qute.psi.QuteSupportForTemplate;
+import com.redhat.devtools.intellij.qute.psi.internal.resolver.ITypeResolver;
+import com.redhat.devtools.intellij.qute.psi.template.datamodel.AbstractAnnotationTypeReferenceDataModelProvider;
+import com.redhat.devtools.intellij.qute.psi.template.datamodel.SearchContext;
+import com.redhat.devtools.intellij.qute.psi.utils.AnnotationUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import com.redhat.qute.commons.datamodel.resolvers.MessageResolverData;
+import com.redhat.qute.commons.datamodel.resolvers.ValueResolverInfo;
+import com.redhat.qute.commons.datamodel.resolvers.ValueResolverKind;
+
+import static com.redhat.devtools.intellij.qute.psi.internal.QuteJavaConstants.*;
+
+/**
+ * Type-safe Message Bundles support.
+ *
+ * @author Angelo ZERR
+ * @see <a href="https://quarkus.io/guides/qute-reference#type-safe-message-bundles">https://quarkus.io/guides/qute-reference#type-safe-message-bundles</a>
+ */
+public class TypeSafeMessageBundlesSupport extends AbstractAnnotationTypeReferenceDataModelProvider {
+
+    private static final String DEFAULT_MESSAGE_NAMESPACE = "msg";
+
+    private static final Logger LOGGER = Logger.getLogger(TemplateGlobalAnnotationSupport.class.getName());
+
+    private static final String[] ANNOTATION_NAMES = {MESSAGE_ANNOTATION};
+
+    @Override
+    protected String[] getAnnotationNames() {
+        return ANNOTATION_NAMES;
+    }
+
+    @Override
+    protected void processAnnotation(PsiElement javaElement, PsiAnnotation annotation, String annotationName, SearchContext context, ProgressIndicator monitor) {
+        if (!(javaElement instanceof PsiMember)) {
+            return;
+        }
+        if (annotation == null) {
+            return;
+        }
+        ITypeResolver typeResolver = QuteSupportForTemplate.createTypeResolver((PsiMember) javaElement, context.getJavaProject());
+        if (javaElement instanceof PsiMethod) {
+            PsiMethod method = (PsiMethod) javaElement;
+            collectResolversForMessage(method, annotation, context.getDataModelProject().getValueResolvers(),
+                    typeResolver);
+        }
+    }
+
+    private void collectResolversForMessage(PsiMethod method, PsiAnnotation messageAnnotation,
+                                            List<ValueResolverInfo> resolvers, ITypeResolver typeResolver) {
+
+        // @MessageBundle
+        // public interface AppMessages {
+        PsiAnnotation messageBundleAnnotation = getMessageBundleAnnotation(method.getContainingClass());
+        String sourceType = method.getContainingClass().getQualifiedName();
+        ValueResolverInfo resolver = new ValueResolverInfo();
+        String namespace = getNamespaceMessage(messageBundleAnnotation);
+        resolver.setNamespace(namespace);
+        resolver.setSourceType(sourceType);
+        resolver.setSignature(typeResolver.resolveSignature(method));
+        resolver.setKind(ValueResolverKind.Message);
+
+        // data message
+        String locale = getLocaleMessage(messageBundleAnnotation);
+        String messageContent = getMessageContent(messageAnnotation);
+        if (locale != null || messageContent != null) {
+            MessageResolverData data = new MessageResolverData();
+            data.setLocale(locale);
+            data.setMessage(messageContent);
+            resolver.setData(data);
+        }
+
+        if (!resolvers.contains(resolver)) {
+            resolvers.add(resolver);
+        }
+
+    }
+
+    private static PsiAnnotation getMessageBundleAnnotation(PsiClass type) {
+        try {
+            return AnnotationUtils.getAnnotation(type, MESSAGE_BUNDLE_ANNOTATION);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error while getting @MessageBundle annotation value.", e);
+            return null;
+        }
+    }
+
+    private static String getNamespaceMessage(PsiAnnotation messageBundleAnnotation) {
+        String namespace = null;
+        try {
+            if (messageBundleAnnotation != null) {
+                namespace = AnnotationUtils.getAnnotationMemberValue(messageBundleAnnotation, VALUE_ANNOTATION_NAME);
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error while getting @MessageBundle#value annotation value.", e);
+            return null;
+        }
+        return StringUtils.isEmpty(namespace) ? DEFAULT_MESSAGE_NAMESPACE : namespace;
+    }
+
+    private static String getLocaleMessage(PsiAnnotation messageBundleAnnotation) {
+        try {
+            if (messageBundleAnnotation != null) {
+                return AnnotationUtils.getAnnotationMemberValue(messageBundleAnnotation,
+                        MESSAGE_BUNDLE_ANNOTATION_LOCALE);
+            }
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error while getting @MessageBundle#locale annotation value.", e);
+            return null;
+        }
+        return null;
+    }
+
+    private static String getMessageContent(PsiAnnotation messageAnnotation) {
+        try {
+            return AnnotationUtils.getAnnotationMemberValue(messageAnnotation, VALUE_ANNOTATION_NAME);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Error while getting @Message#value annotation value.", e);
+            return null;
+        }
+    }
+}
--- src/test/java/com/redhat/devtools/intellij/qute/psi/QuteAssert.java
+++ src/test/java/com/redhat/devtools/intellij/qute/psi/QuteAssert.java
@@ -1,85 +1,97 @@
 /*******************************************************************************
-* Copyright (c) 2023 Red Hat Inc. and others.
-* All rights reserved. This program and the accompanying materials
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v20.html
-*
-* SPDX-License-Identifier: EPL-2.0
-*
-* Contributors:
-*     Red Hat Inc. - initial API and implementation
-*******************************************************************************/
+ * Copyright (c) 2023 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
 package com.redhat.devtools.intellij.qute.psi;
+
+import com.redhat.qute.commons.datamodel.DataModelParameter;
+import com.redhat.qute.commons.datamodel.DataModelTemplate;
+import com.redhat.qute.commons.datamodel.resolvers.MessageResolverData;
+import com.redhat.qute.commons.datamodel.resolvers.ValueResolverInfo;
+import org.junit.Assert;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
-import com.redhat.qute.commons.datamodel.DataModelTemplate;
-import org.junit.Assert;
-
-import com.redhat.qute.commons.datamodel.DataModelParameter;
-import com.redhat.qute.commons.datamodel.resolvers.ValueResolverInfo;
-
 /**
  * Qute assert.
- * 
- * @author Angelo ZERR
  *
+ * @author Angelo ZERR
  */
 public class QuteAssert {
 
-	public static void assertValueResolver(String namespace, String signature, String sourceType,
-			List<ValueResolverInfo> resolvers) {
-		assertValueResolver(namespace, signature, sourceType, null, resolvers);
-	}
+    public static void assertValueResolver(String namespace, String signature, String sourceType,
+                                           List<ValueResolverInfo> resolvers) {
+        assertValueResolver(namespace, signature, sourceType, null, resolvers);
+    }
 
-	public static void assertValueResolver(String namespace, String signature, String sourceType, String named,
-			List<ValueResolverInfo> resolvers) {
-		assertValueResolver(namespace, signature, sourceType, named, false, resolvers);
-	}
+    public static void assertValueResolver(String namespace, String signature, String sourceType, String named,
+                                           List<ValueResolverInfo> resolvers) {
+        assertValueResolver(namespace, signature, sourceType, named, false, resolvers);
+    }
 
-	public static void assertValueResolver(String namespace, String signature, String sourceType, String named,
-			boolean globalVariable, List<ValueResolverInfo> resolvers) {
-		Optional<ValueResolverInfo> result = resolvers.stream()
-				.filter(r -> signature.equals(r.getSignature()) && Objects.equals(namespace, r.getNamespace()))
-				.findFirst();
-		Assert.assertFalse("Find '" + signature + "' value resolver.", result.isEmpty());
-		ValueResolverInfo resolver = result.get();
-		Assert.assertEquals(namespace, resolver.getNamespace());
-		Assert.assertEquals(signature, resolver.getSignature());
-		Assert.assertEquals(sourceType, resolver.getSourceType());
-		Assert.assertEquals(globalVariable, resolver.isGlobalVariable());
-	}
+    public static ValueResolverInfo assertValueResolver(String namespace, String signature, String sourceType, String named,
+                                                        boolean globalVariable, List<ValueResolverInfo> resolvers) {
+        Optional<ValueResolverInfo> result = resolvers.stream()
+                .filter(r -> signature.equals(r.getSignature()) && Objects.equals(namespace, r.getNamespace()))
+                .findFirst();
+        Assert.assertFalse("Find '" + signature + "' value resolver.", result.isEmpty());
+        ValueResolverInfo resolver = result.get();
+        Assert.assertEquals(namespace, resolver.getNamespace());
+        Assert.assertEquals(signature, resolver.getSignature());
+        Assert.assertEquals(sourceType, resolver.getSourceType());
+        Assert.assertEquals(globalVariable, resolver.isGlobalVariable());
+        return resolver;
+    }
 
-	public static void assertNotValueResolver(String namespace, String signature, String sourceType, String named,
-			List<ValueResolverInfo> resolvers) {
-		assertNotValueResolver(namespace, signature, sourceType, named, false, resolvers);
-	}
+    public static void assertNotValueResolver(String namespace, String signature, String sourceType, String named,
+                                              List<ValueResolverInfo> resolvers) {
+        assertNotValueResolver(namespace, signature, sourceType, named, false, resolvers);
+    }
 
-	public static void assertNotValueResolver(String namespace, String signature, String sourceType, String named,
-			boolean globalVariable, List<ValueResolverInfo> resolvers) {
-		Optional<ValueResolverInfo> result = resolvers.stream().filter(r -> signature.equals(r.getSignature()))
-				.findFirst();
-		Assert.assertTrue("Find '" + signature + "' value resolver.", result.isEmpty());
-	}
+    public static void assertNotValueResolver(String namespace, String signature, String sourceType, String named,
+                                              boolean globalVariable, List<ValueResolverInfo> resolvers) {
+        Optional<ValueResolverInfo> result = resolvers.stream().filter(r -> signature.equals(r.getSignature()))
+                .findFirst();
+        Assert.assertTrue("Find '" + signature + "' value resolver.", result.isEmpty());
+    }
 
-	public static void assertParameter(String key, String sourceType, boolean dataMethodInvocation,
-			List<DataModelParameter> parameters, int index) {
-		DataModelParameter parameter = parameters.get(index);
-		Assert.assertEquals(key, parameter.getKey());
-		Assert.assertEquals(sourceType, parameter.getSourceType());
-		Assert.assertEquals(dataMethodInvocation, parameter.isDataMethodInvocation());
-	}
+    public static void assertMessageResolverData(String locale, String messageContent, ValueResolverInfo message) {
+        if (locale == null && messageContent == null) {
+            Assert.assertNull(message.getData());
+        } else {
+            Assert.assertNotNull(message.getData());
+            Assert.assertTrue(message.getData() instanceof MessageResolverData);
+            MessageResolverData data = (MessageResolverData) message.getData();
+            Assert.assertEquals(locale, data.getLocale());
+            Assert.assertEquals(messageContent, data.getMessage());
+        }
+    }
 
-	public static void assertParameter(String key, String sourceType, boolean dataMethodInvocation,
-									   DataModelTemplate<DataModelParameter> template) {
-		DataModelParameter parameter = template.getParameter(key);
-		Assert.assertNotNull(parameter);
-		Assert.assertEquals(key, parameter.getKey());
-		Assert.assertEquals(sourceType, parameter.getSourceType());
-		Assert.assertEquals(dataMethodInvocation, parameter.isDataMethodInvocation());
-	}
+    public static void assertParameter(String key, String sourceType, boolean dataMethodInvocation,
+                                       List<DataModelParameter> parameters, int index) {
+        DataModelParameter parameter = parameters.get(index);
+        Assert.assertEquals(key, parameter.getKey());
+        Assert.assertEquals(sourceType, parameter.getSourceType());
+        Assert.assertEquals(dataMethodInvocation, parameter.isDataMethodInvocation());
+    }
+
+    public static void assertParameter(String key, String sourceType, boolean dataMethodInvocation,
+                                       DataModelTemplate<DataModelParameter> template) {
+        DataModelParameter parameter = template.getParameter(key);
+        Assert.assertNotNull(parameter);
+        Assert.assertEquals(key, parameter.getKey());
+        Assert.assertEquals(sourceType, parameter.getSourceType());
+        Assert.assertEquals(dataMethodInvocation, parameter.isDataMethodInvocation());
+    }
 
 
 }
