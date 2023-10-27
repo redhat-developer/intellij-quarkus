72fffcffea81c9c85fa462bf6fc7ca0f5a275f88
Fred Bricon <fbricon@gmail.com>
2023-10-27 18:06:50 +0200

    build: fix version parsing
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- get_timestamp_version.sh
+++ get_timestamp_version.sh
@@ -3,8 +3,8 @@
 # Path to your gradle.properties file
 GRADLE_PROPERTIES_FILE="gradle.properties"
 
-# Read the current version from gradle.properties
-CURRENT_VERSION=$(grep "pluginVersion=" "$GRADLE_PROPERTIES_FILE" | cut -d'=' -f2)
+# Read the current version from gradle.properties, ignoring spaces around the equals sign
+CURRENT_VERSION=$(grep -E "pluginVersion\s*=\s*" "$GRADLE_PROPERTIES_FILE" | sed -E 's/.*=\s*([^[:space:]]*).*/\1/')
 
 # Extract the version part before any hyphen
 BASE_VERSION=$(echo "$CURRENT_VERSION" | awk -F'-' '{print $1}')
@@ -13,7 +13,7 @@ BASE_VERSION=$(echo "$CURRENT_VERSION" | awk -F'-' '{print $1}')
 TIMESTAMP=$(date +'%Y%m%d-%H%M%S')
 NEW_VERSION="${BASE_VERSION}-$TIMESTAMP"
 
-# Use awk to update the gradle.properties file
-awk -v new_version="$NEW_VERSION" '/pluginVersion=/{sub(/=.*/, "=" new_version)}1' "$GRADLE_PROPERTIES_FILE" > tmpfile && mv tmpfile "$GRADLE_PROPERTIES_FILE"
+# Use awk to update the gradle.properties file, allowing spaces around the equals sign
+awk -v new_version="$NEW_VERSION" '/pluginVersion\s*=\s*/{sub(/=.*/, "=" new_version)}1' "$GRADLE_PROPERTIES_FILE" > tmpfile && mv tmpfile "$GRADLE_PROPERTIES_FILE"
 
 echo $NEW_VERSION
