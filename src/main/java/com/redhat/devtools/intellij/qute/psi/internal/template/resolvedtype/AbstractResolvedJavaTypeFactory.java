4a555219e36b62b8f7c973b0b99a4f28fb2b5b71
azerr <azerr@redhat.com>
2024-06-04 17:28:37 +0200

    fix: adapt to change in ProcessCanceledException hierarchy in 2024. (2)
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/qute/psi/internal/template/resolvedtype/AbstractResolvedJavaTypeFactory.java
+++ src/main/java/com/redhat/devtools/intellij/qute/psi/internal/template/resolvedtype/AbstractResolvedJavaTypeFactory.java
@@ -136,7 +136,11 @@ public abstract class AbstractResolvedJavaTypeFactory implements IResolvedJavaTy
 			if (!type.isInterface() && !method.getModifierList().hasExplicitModifier(PsiModifier.PUBLIC)) {
 				return false;
 			}
-		} catch (IndexNotReadyException | ProcessCanceledException | CancellationException e) {
+		} catch (ProcessCanceledException e) {
+			//Since 2024.2 ProcessCanceledException extends CancellationException so we can't use multicatch to keep backward compatibility
+			//TODO delete block when minimum required version is 2024.2
+			throw e;
+		} catch (IndexNotReadyException | CancellationException e) {
 			throw e;
 		} catch (Exception e) {
 			LOGGER.log(Level.WARNING, "Error while checking if '" + method.getName() + "' is valid.", e);
