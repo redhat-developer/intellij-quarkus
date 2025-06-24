53a772539f324fa1b56490cde5f76c6e747fc816
Stephane Bouchet <sbouchet@redhat.com>
2025-06-24 16:05:56 +0200

    chore: bump common-ui-tests to 0.4.5-SNAPSHOT
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

---
+++ src/it/java/org/jboss/tools/intellij/quarkus/tests/GradleTest.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.intellij.quarkus.tests;
+
+import com.redhat.devtools.intellij.commonuitest.fixtures.dialogs.FlatWelcomeFrame;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.BuildView;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.ToolWindowPane;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.buildtoolpane.GradleBuildToolPane;
+import com.redhat.devtools.intellij.commonuitest.utils.project.CreateCloseUtils;
+import org.jboss.tools.intellij.quarkus.utils.BuildTool;
+import org.jboss.tools.intellij.quarkus.utils.EndpointURLType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Basic Gradle Quarkus tests
+ *
+ */
+public class GradleTest extends AbstractQuarkusTest {
+    private static final String NEW_QUARKUS_GRADLE_PROJECT_NAME = "code-with-quarkus-gradle";
+    private static final String JAVA_VERSION = "17";
+
+    @AfterEach
+    public void finishTestRun() {
+        CreateCloseUtils.closeProject(remoteRobot);
+        FlatWelcomeFrame flatWelcomeFrame = remoteRobot.find(FlatWelcomeFrame.class, Duration.ofSeconds(10));
+        flatWelcomeFrame.clearExceptions();
+        flatWelcomeFrame.clearWorkspace();
+    }
+
+    @Test
+    public void createBuildQuarkusGradleTest() throws IOException {
+        createQuarkusProject(remoteRobot, NEW_QUARKUS_GRADLE_PROJECT_NAME, BuildTool.GRADLE, EndpointURLType.DEFAULT, JAVA_VERSION);
+        ToolWindowPane toolWindowPane = remoteRobot.find(ToolWindowPane.class, Duration.ofSeconds(10));
+        toolWindowPane.openGradleBuildToolPane();
+        GradleBuildToolPane gradleBuildToolPane = toolWindowPane.find(GradleBuildToolPane.class, Duration.ofSeconds(10));
+        gradleBuildToolPane.buildProject();
+        boolean isBuildSuccessful = toolWindowPane.find(BuildView.class, Duration.ofSeconds(10)).isBuildSuccessful();
+        assertTrue(isBuildSuccessful, "The build should be successful but is not.");
+    }
+
+}
---
+++ src/it/java/org/jboss/tools/intellij/quarkus/tests/MavenTest.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package org.jboss.tools.intellij.quarkus.tests;
+
+import com.redhat.devtools.intellij.commonuitest.fixtures.dialogs.FlatWelcomeFrame;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.BuildView;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.ToolWindowPane;
+import com.redhat.devtools.intellij.commonuitest.fixtures.mainidewindow.toolwindowspane.buildtoolpane.MavenBuildToolPane;
+import com.redhat.devtools.intellij.commonuitest.utils.project.CreateCloseUtils;
+import org.jboss.tools.intellij.quarkus.utils.BuildTool;
+import org.jboss.tools.intellij.quarkus.utils.EndpointURLType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Basic Maven Quarkus tests
+ *
+ * @author zcervink@redhat.com
+ */
+public class MavenTest extends AbstractQuarkusTest {
+    private static final String NEW_QUARKUS_MAVEN_PROJECT_NAME = "code-with-quarkus-maven";
+    private static final String JAVA_VERSION_FOR_QUARKUS_PROJECT = "17";
+
+    @AfterEach
+    public void finishTestRun() {
+        CreateCloseUtils.closeProject(remoteRobot);
+        FlatWelcomeFrame flatWelcomeFrame = remoteRobot.find(FlatWelcomeFrame.class, Duration.ofSeconds(10));
+        flatWelcomeFrame.clearExceptions();
+        flatWelcomeFrame.clearWorkspace();
+    }
+
+    @Test
+    public void createBuildQuarkusMavenTest() throws IOException {
+        createQuarkusProject(remoteRobot, NEW_QUARKUS_MAVEN_PROJECT_NAME, BuildTool.MAVEN, EndpointURLType.DEFAULT, JAVA_VERSION_FOR_QUARKUS_PROJECT);
+        ToolWindowPane toolWindowPane = remoteRobot.find(ToolWindowPane.class, Duration.ofSeconds(10));
+        toolWindowPane.openMavenBuildToolPane();
+        MavenBuildToolPane mavenBuildToolPane = toolWindowPane.find(MavenBuildToolPane.class, Duration.ofSeconds(10));
+        mavenBuildToolPane.buildProject("verify", "code-with-quarkus");
+        boolean isBuildSuccessful = toolWindowPane.find(BuildView.class, Duration.ofSeconds(10)).isBuildSuccessful();
+        assertTrue(isBuildSuccessful, "The build should be successful but is not.");
+    }
+
+
+}
