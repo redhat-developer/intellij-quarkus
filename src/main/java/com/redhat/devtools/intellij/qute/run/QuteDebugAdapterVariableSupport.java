802379f4e71f207d2555fb0a23bbec6517360209
azerr <azerr@redhat.com>
2025-08-17 19:06:50 +0200

    fix: fix prefix completion with Qute debugger
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/qute/run/QuteDebugAdapterVariableSupport.java
+++ src/main/java/com/redhat/devtools/intellij/qute/run/QuteDebugAdapterVariableSupport.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at https://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
 package com.redhat.devtools.intellij.qute.run;
 
 import com.redhat.devtools.lsp4ij.dap.client.variables.providers.DebugVariablePositionProvider;
@@ -8,6 +18,9 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Collection;
 import java.util.Collections;
 
+/**
+ * Qute debug adapter variable support.
+ */
 public class QuteDebugAdapterVariableSupport extends DebugAdapterVariableSupport {
 
     @Override
