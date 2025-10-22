0bea5733155b46ae55d0182926e5164efe28939b
azerr <azerr@redhat.com>
2025-10-22 11:00:25 +0200

    feat: collect proper variable for hover with Qute debugger
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/qute/run/QuteDebugAdapterVariableSupport.java
+++ src/main/java/com/redhat/devtools/intellij/qute/run/QuteDebugAdapterVariableSupport.java
@@ -10,10 +10,25 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.qute.run;
 
+import com.intellij.extapi.psi.ASTWrapperPsiElement;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.source.tree.CompositeElement;
+import com.redhat.devtools.intellij.qute.lang.QuteLanguage;
+import com.redhat.devtools.intellij.qute.lang.psi.QuteElementType;
+import com.redhat.devtools.intellij.qute.lang.psi.QuteElementTypes;
+import com.redhat.devtools.intellij.qute.lang.psi.QuteToken;
+import com.redhat.devtools.intellij.qute.lang.psi.QuteTokenType;
+import com.redhat.devtools.lsp4ij.LSPIJUtils;
 import com.redhat.devtools.lsp4ij.dap.client.variables.providers.DebugVariablePositionProvider;
 import com.redhat.devtools.lsp4ij.dap.client.variables.providers.HighlighterDebugVariablePositionProvider;
 import com.redhat.devtools.lsp4ij.dap.descriptors.DebugAdapterVariableSupport;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -27,4 +42,23 @@ public class QuteDebugAdapterVariableSupport extends DebugAdapterVariableSupport
     public @NotNull Collection<DebugVariablePositionProvider> getDebugVariablePositionProvider() {
         return Collections.singletonList(new QuteDebugVariablePositionProvider());
     }
+
+    @Override
+    protected @Nullable TextRange getTextRange(@NotNull Project project, @NotNull VirtualFile file, @NotNull Document document, int offset, boolean sideEffectsAllowed) {
+        PsiFile psiFile = LSPIJUtils.getPsiFile(file, project);
+        if (psiFile == null) {
+            return null;
+        }
+        var element = psiFile.findElementAt(offset);
+        if (element == null || !QuteLanguage.INSTANCE.equals(element.getLanguage())) {
+            return null;
+        }
+        if (element instanceof QuteToken quteToken) {
+            return quteToken.getTextRangeInExpression();
+        }
+        return null;
+    }
+
 }
+
+
