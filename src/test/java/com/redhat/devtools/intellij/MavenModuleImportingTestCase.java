3f1abb9fe81f36fcb678a4567d62b3fa59c414ea
Stephane Bouchet <sbouchet@redhat.com>
2025-10-29 10:27:32 +0100

    chore: fix tests
    Signed-off-by: Stephane Bouchet <sbouchet@redhat.com>

--- src/test/java/com/redhat/devtools/intellij/MavenModuleImportingTestCase.java
+++ src/test/java/com/redhat/devtools/intellij/MavenModuleImportingTestCase.java
@@ -18,34 +18,33 @@ import com.intellij.openapi.roots.LanguageLevelProjectExtension;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.pom.java.LanguageLevel;
+import com.intellij.testFramework.IndexingTestUtil;
 import com.intellij.testFramework.builders.JavaModuleFixtureBuilder;
 import com.intellij.testFramework.fixtures.*;
 import com.redhat.devtools.intellij.lsp4mp4ij.psi.core.utils.IPsiUtils;
 import com.redhat.devtools.intellij.lsp4mp4ij.psi.internal.core.ls.PsiUtilsLSImpl;
-import kotlin.coroutines.EmptyCoroutineContext;
-import kotlinx.coroutines.BuildersKt;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public abstract class MavenModuleImportingTestCase extends MavenImportingTestCase {
 
-  protected TestFixtureBuilder<IdeaProjectTestFixture> myProjectBuilder;
+    protected TestFixtureBuilder<IdeaProjectTestFixture> myProjectBuilder;
 
-  protected void setUpFixtures() throws Exception {
-    myProjectBuilder = IdeaTestFixtureFactory.getFixtureFactory().createFixtureBuilder(getName());
-    final JavaTestFixtureFactory factory = JavaTestFixtureFactory.getFixtureFactory();
-    myProjectBuilder.addModule(JavaModuleFixtureBuilder.class);
-    IdeaProjectTestFixture myFixture = factory.createCodeInsightFixture(myProjectBuilder.getFixture());
-    myFixture.setUp();
-    setTestFixture(myFixture);
-    LanguageLevelProjectExtension.getInstance(myFixture.getProject()).setLanguageLevel(LanguageLevel.JDK_17);
-  }
+    protected void setUpFixtures() throws Exception {
+        myProjectBuilder = IdeaTestFixtureFactory.getFixtureFactory().createFixtureBuilder(getName());
+        final JavaTestFixtureFactory factory = JavaTestFixtureFactory.getFixtureFactory();
+        myProjectBuilder.addModule(JavaModuleFixtureBuilder.class);
+        IdeaProjectTestFixture myFixture = factory.createCodeInsightFixture(myProjectBuilder.getFixture());
+        myFixture.setUp();
+        setTestFixture(myFixture);
+        LanguageLevelProjectExtension.getInstance(myFixture.getProject()).setLanguageLevel(LanguageLevel.JDK_17);
+    }
 
-  private static int counter = 0;
+    private static int counter = 0;
 
   /**
    * Create a new module into the test project from existing project folder.
@@ -55,49 +54,25 @@ public abstract class MavenModuleImportingTestCase extends MavenImportingTestCas
    */
   protected List<Module> createMavenModules(List<File> projectDirs) throws Exception {
     Project project = getTestFixture().getProject();
+    List<VirtualFile> pomFiles = new ArrayList<>();
     for(File projectDir : projectDirs) {
       File moduleDir = new File(project.getBasePath(), projectDir.getName() + counter++);
       FileUtils.copyDirectory(projectDir, moduleDir);
       VirtualFile pomFile = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(moduleDir).findFileByRelativePath("pom.xml");
-      BuildersKt.runBlocking(
-          EmptyCoroutineContext.INSTANCE,
-          (scope, continuation) -> importProjectAsync(pomFile,continuation)
-      );
+      pomFiles.add(pomFile);
     }
+    // Calling the non-suspending importProjects method instead of the Kotlin suspending function
+    // importProjectsAsync(file: VirtualFile) to prevent blocking unit tests starting from IntelliJ version 2024.2.
+    importProjects(pomFiles.toArray(VirtualFile[]::new));
 
     Module[] modules = ModuleManager.getInstance(project).getModules();
     for(Module module : modules) {
       setupJdkForModule(module.getName());
     }
+    // Starting from IntelliJ 2024.2, indexing runs asynchronously in a background thread, https://plugins.jetbrains.com/docs/intellij/testing-faq.html#how-to-handle-indexing.
+    // Use the following method to ensure indexes are fully populated before proceeding.
+    IndexingTestUtil.waitUntilIndexesAreReady(project);
     return Arrays.stream(modules).skip(1).toList();
-  }
-
-  protected Module createMavenModule(File projectDir) throws Exception {
-    List<Module> modules = createMavenModules(Collections.singletonList(projectDir));
-    return modules.get(modules.size() - 1);
-  }
-
-  /**
-   * Create a new module into the test project from existing in memory POM.
-   *
-   * @param name the new module name
-   * @param xml the project POM
-   * @return the created module
-   */
-  protected Module createMavenModule(String name, String xml) throws Exception {
-    Module module = getTestFixture().getModule();
-    File moduleDir = new File(module.getModuleFilePath()).getParentFile();
-    VirtualFile pomFile = createPomFile(LocalFileSystem.getInstance().findFileByIoFile(moduleDir), xml);
-    BuildersKt.runBlocking(
-        EmptyCoroutineContext.INSTANCE,
-        (scope, continuation) -> importProjectAsync(pomFile,continuation)
-    );
-    Module[] modules = ModuleManager.getInstance(getTestFixture().getProject()).getModules();
-    if (modules.length > 0) {
-      module = modules[modules.length - 1];
-      setupJdkForModule(module.getName());
-    }
-    return module;
   }
 
   protected IPsiUtils getJDTUtils() {
