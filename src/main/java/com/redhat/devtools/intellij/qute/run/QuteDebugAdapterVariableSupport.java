b27a098defe2bf4aa1566b4128714470526f49c7
azerr <azerr@redhat.com>
2025-11-04 16:17:03 +0100

    feat: Support Qute debugger hover with JetBrains Qute support
    Signed-off-by: azerr <azerr@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/qute/run/QuteDebugAdapterVariableSupport.java
+++ src/main/java/com/redhat/devtools/intellij/qute/run/QuteDebugAdapterVariableSupport.java
@@ -10,22 +10,17 @@
  ******************************************************************************/
 package com.redhat.devtools.intellij.qute.run;
 
-import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.impl.source.tree.CompositeElement;
+import com.intellij.psi.tree.IElementType;
 import com.redhat.devtools.intellij.qute.lang.QuteLanguage;
-import com.redhat.devtools.intellij.qute.lang.psi.QuteElementType;
-import com.redhat.devtools.intellij.qute.lang.psi.QuteElementTypes;
 import com.redhat.devtools.intellij.qute.lang.psi.QuteToken;
-import com.redhat.devtools.intellij.qute.lang.psi.QuteTokenType;
 import com.redhat.devtools.lsp4ij.LSPIJUtils;
 import com.redhat.devtools.lsp4ij.dap.client.variables.providers.DebugVariablePositionProvider;
-import com.redhat.devtools.lsp4ij.dap.client.variables.providers.HighlighterDebugVariablePositionProvider;
 import com.redhat.devtools.lsp4ij.dap.descriptors.DebugAdapterVariableSupport;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -37,6 +32,13 @@ import java.util.Collections;
  * Qute debug adapter variable support.
  */
 public class QuteDebugAdapterVariableSupport extends DebugAdapterVariableSupport {
+
+    public static final String QUTE_JETBRAINS_IDENTIFIER = "IDENTIFIER";
+    public static final String QUTE_JETBRAINS_IDENTIFIER_EXPR = "IDENTIFIER_EXPR";
+    public static final String QUTE_JETBRAINS_REFERENCE_EXPR = "REFERENCE_EXPR";
+    public static final String QUTE_JETBRAINS_NAMESPACE_EXPR = "NAMESPACE_EXPR";
+    public static final String QUTE_JETBRAINS_QUALIFIER = "QUALIFIER";
+    public static final String QUTE_JETBRAINS_CALL_EXPR = "CALL_EXPR";
 
     @Override
     public @NotNull Collection<DebugVariablePositionProvider> getDebugVariablePositionProvider() {
@@ -59,7 +61,64 @@ public class QuteDebugAdapterVariableSupport extends DebugAdapterVariableSupport
             return quteToken.getTextRangeInExpression();
         }
         // Qute Token coming from JetBrains Quarkus
-        return element.getTextRange();
+        var tokenType = getTokenType(element);
+        if (isTokenType(tokenType, QUTE_JETBRAINS_IDENTIFIER)) {
+            var parent = element.getParent();
+            tokenType = getTokenType(parent);
+            if (isTokenType(tokenType, QUTE_JETBRAINS_IDENTIFIER_EXPR)) {
+                // Object part:
+                // item
+                // or uri:Item
+                parent = parent.getParent();
+                tokenType = getTokenType(parent);
+                if (isTokenType(tokenType, QUTE_JETBRAINS_REFERENCE_EXPR)) {
+                    var nsParent = parent.getParent();
+                    tokenType = getTokenType(nsParent);
+                    if (isTokenType(tokenType, QUTE_JETBRAINS_NAMESPACE_EXPR)) {
+                        // uri:Item
+                        return new TextRange(nsParent.getTextRange().getStartOffset(), element.getTextRange().getEndOffset());
+                    }
+                }
+                // item
+                return element.getTextRange();
+            } else if (isTokenType(tokenType, QUTE_JETBRAINS_QUALIFIER)) {
+                // Property or Method part
+                // item.foo
+                // item.bar()
+                parent = parent.getParent();
+                tokenType = getTokenType(parent);
+                if (isTokenType(tokenType, QUTE_JETBRAINS_REFERENCE_EXPR)) {
+                    var nsParent = parent.getParent();
+                    tokenType = getTokenType(nsParent);
+                    if (isTokenType(tokenType, QUTE_JETBRAINS_NAMESPACE_EXPR)) {
+                        parent = nsParent;
+                    }
+                    var callExprParent = parent.getParent();
+                    tokenType = getTokenType(callExprParent);
+                    if (isTokenType(tokenType, QUTE_JETBRAINS_CALL_EXPR)) {
+                        // Method part
+                        // item.bar()
+                        return callExprParent.getTextRange();
+                    }
+                }
+                // Property part
+                // item.foo
+                return parent.getTextRange();
+            }
+        }
+        return null;
+    }
+
+    private static @Nullable IElementType getTokenType(@Nullable PsiElement element) {
+        if (element == null) {
+            return null;
+        }
+        var node = element.getNode();
+        return node != null ? node.getElementType() : null;
+    }
+
+    private static boolean isTokenType(@Nullable IElementType tokenType, @NotNull String tokenName) {
+        return tokenType != null && tokenName.equals(tokenType.getDebugName());
     }
 
 }
