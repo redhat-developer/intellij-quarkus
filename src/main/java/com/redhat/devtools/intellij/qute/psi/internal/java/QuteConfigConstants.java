787527a3135edad6d04b7e4876662d7832d072ab
azerr <azerr@redhat.com>
2026-02-25 10:55:31 +0100

    fix: Qute : false positive on non-html template
    Fixes #1554
    
    Signed-off-by: azerr <azerr@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/qute/psi/internal/java/QuteConfigConstants.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2026 Red Hat Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package com.redhat.devtools.intellij.qute.psi.internal.java;
+
+/**
+ * Qute config constants.
+ */
+public class QuteConfigConstants {
+
+    public static final String QUARKUS_QUTE_SUFFIXES = "quarkus.qute.suffixes";
+
+    private QuteConfigConstants() {
+
+    }
+}
